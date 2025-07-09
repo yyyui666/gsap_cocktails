4e7c53b9c271403c0537cfb45651166c4aceea24
Yui <montyjack04@gmail.com>
2025-07-10 01:10:56 +0200

    impliment the about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import gsap from 'gsap'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
+import About from './components/About'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   )
 }
