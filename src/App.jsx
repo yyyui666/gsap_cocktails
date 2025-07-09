5aab204b994020ffcf733944bfb88dee0a566d32
Yui <montyjack04@gmail.com>
2025-07-10 01:37:48 +0200

    Impliment the art section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
 import About from './components/About'
+import Art from './components/Art'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -15,6 +16,7 @@ function App() {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   )
 }
