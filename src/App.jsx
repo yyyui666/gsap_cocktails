19541420baea8d5f5574b57ff831ba6696209492
Yui <montyjack04@gmail.com>
2025-07-10 02:23:40 +0200

    impliment contact section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Cocktails from './components/Cocktails'
 import About from './components/About'
 import Art from './components/Art'
 import Menu from './components/Menu'
+import Contact from './components/Contact'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -19,6 +20,7 @@ function App() {
       <About />
       <Art />
       <Menu />
+      <Contact />
     </main>
   )
 }
