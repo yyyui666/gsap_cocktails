1f3510704f258844e61757e1b397dfe6d1178c79
Yui <montyjack04@gmail.com>
2025-07-10 02:05:05 +0200

    impliment the menu section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
 import About from './components/About'
 import Art from './components/Art'
+import Menu from './components/Menu'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -17,6 +18,7 @@ function App() {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   )
 }
